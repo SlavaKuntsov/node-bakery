1e28237c41976bbaadbf35618ed870dd3c2f9b2d
Slava <kuncovs19@gmail.com>
2023-12-22 03:02:03 +0300

    add api cors

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const app = express()
 
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
+	res.header('Access-Control-Allow-Origin', 'https://bakery-backend-9dyd.onrender.com')
 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
 	res.header('Access-Control-Allow-Headers', 'Content-Type')
 	next()
