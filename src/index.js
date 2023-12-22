3eaf3d8314412a61b310bc399c5f3a9ca526453f
Slava <kuncovs19@gmail.com>
2023-12-22 03:04:04 +0300

    add new api cors

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const app = express()
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
 	res.header('Access-Control-Allow-Origin', 'https://bakery-backend-9dyd.onrender.com')
+	res.setHeader('Access-Control-Allow-Origin', 'https://beloruskyismak.vercel.app');
 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
 	res.header('Access-Control-Allow-Headers', 'Content-Type')
 	next()
